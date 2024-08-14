81c0eacb4bffc06472d9bfcdde0df66e52b20e83
teaplusottp <120555692+teaplusottp@users.noreply.github.com>
2024-08-14 22:52:30 +0700

    Add files via upload

---
+++ UIProject (1)/plugin/test.py
@@ -0,0 +1,11 @@
+
+import json
+
+def read_json_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return [video["watch_url"].replace("watch?v=", "embed/") for video in data]
+
+# file="D:\\code\\python\\UIProject\\Metadata.json"
+
+# print(read_json_file(file))
